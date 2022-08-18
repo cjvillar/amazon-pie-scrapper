a4a3ebd47b2646fed18989da823ba23e16d35b88
Chris Villarreal <chrisvillarreal84@gmail.com>
2022-08-17 21:56:47 -0700

    updating

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Amazon-Pie-Scrapper
-Get the price of Raspberry Pi 4s scrapped from amazon (US).
+# Amazon-Pie-Scrapper (Work in Progress)
+Get the price of Raspberry Pi 4s scrapped from amazon (US). 
 
 ## Installation
 
@@ -10,20 +10,7 @@ npm install
 ```bash
 npm run start
 ```
-Output Example:
-```bash
-[
-  '265.99', '269.99', '124.99',
-  '191.90', '182.00', '69.95',
-  '59.95',  '169.95', '49.95',
-  '129.95', '185.00', '134.79',
-  '159.97', '319.99', '5.39',
-  '159.99', '219.99', '159.97',
-  '248.99', '265.99', '265.99',
-  '285.99', '124.29', '199.99',
-  '239.99', '59.95',  '169.95'
-]
-```
+Output: Pie_Price.json
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- pageScraper.js
+++ pageScraper.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const scraperObject = {
   url: "https://www.amazon.com/s?k=raspberry+pi+4&crid=1PS9ECP3EE5TL&sprefix=raspberry+pi+4%2Caps%2C139&ref=nb_sb_noss_1",
   async scraper(browser) {
@@ -7,20 +8,35 @@ const scraperObject = {
 
     const whole_price = await page.evaluate(() =>
       Array.from(document.querySelectorAll("span.a-price-whole")).map(
-        (wholePrice => wholePrice.innerText.replace('\n.',''))
+        (wholePrice) => wholePrice.innerText.replace("\n.", "")
       )
     );
 
     const fraction_price = await page.evaluate(() =>
       Array.from(document.querySelectorAll("span.a-price-fraction")).map(
-        (fractionPrice => fractionPrice.innerText)
+        (fractionPrice) => fractionPrice.innerText
       )
     );
 
-    const price = whole_price.map((left, idx) => [left.concat('.',fraction_price[idx])]);
+    const price = whole_price.map((left, idx) => [
+      left.concat(".", fraction_price[idx]),
+    ]);
     const merged_prices = [].concat.apply([], price);
-    
-    console.log(merged_prices)
+    const date = new Date();
+    const json_data = {'Prices':merged_prices, 'date': date};
+
+    console.log(merged_prices);
+    fs.writeFile(
+      "./pie_price.json",
+      JSON.stringify(json_data, null, 3),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Saved to JSON file");
+      }
+    );
 
     await browser.close();
   },
