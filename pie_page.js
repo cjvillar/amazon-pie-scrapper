a0ae4c66e396b80bceff76fbea04d4b7280d064d
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-05-09 15:09:18 -0700

    updating index.html and pie_page.js

--- pie_page.js
+++ pie_page.js
@@ -1,29 +1,29 @@
 fetch("https://www.cjvillarreal.com/amazon-pie-scrapper/pie_price.json")
-.then((response) => {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error("NETWORK RESPONSE ERROR");
-  }
-})
-.then((data) => {
-  console.log(data);
-  displayPrice(data);
-})
-.catch((error) => console.error("FETCH ERROR:", error));
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    displayPrice(data);
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
 
 function displayPrice(data) {
-const Pies = data.Pies;
-const Date = data.Date;
-const pieDiv = document.getElementById("Pie");
-const dateDiv = document.getElementById("Date");
-const Header = document.createElement("h3");
-Header.innerHTML = Date;
-dateDiv.appendChild(Header);
-const pieLength = Pies.length;
-for (let i = 0; i < pieLength; i++) {
-  const listPies = document.createElement("ul");
-  listPies.innerHTML = (Pies[i]["Price"] + ", " +Pies[i]["Product"]);     
-  pieDiv.appendChild(listPies)
-}
+  const pies = data.AMZ.concat(data.CANAKIT);
+  const date = new Date(data.Date);
+  const pieDiv = document.getElementById("Pie");
+  const dateDiv = document.getElementById("Date");
+  const header = document.createElement("h3");
+  header.innerHTML = date.toLocaleDateString();
+  dateDiv.appendChild(header);
+  const pieLength = pies.length;
+  for (let i = 0; i < pieLength; i++) {
+    const listPies = document.createElement("ul");
+    listPies.innerHTML = `${pies[i].Price}, ${pies[i].Product}`;
+    pieDiv.appendChild(listPies);
+  }
 }
